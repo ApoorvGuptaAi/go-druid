1476564c3792e14d82a62d0ac9117878e32abb0f
Apoorv Gupta <apoorv@truera.com>
2023-07-04 11:25:54 +0000

    Found error

--- builder/datasource/data_source_test.go
+++ builder/datasource/data_source_test.go
@@ -15,3 +15,15 @@ func TestLoadUnsupportedType(t *testing.T) {
 	assert.NotNil(err, "error should not be nil")
 	assert.Error(err, "unsupported datasource type")
 }
+
+func TestLoadQuery(t *testing.T) {
+	assert := assert.New(t)
+
+        var qry string
+
+	qry = `{     "type": "query",     "query": {         "dataSource": {             "type": "table",             "name": "multi_tenant_join_pred_label_regression"         },         "dimensions": [{             "type": "default",             "dimension": "__truera_model_id__",             "outputType": "STRING"         }],         "intervals": {             "type": "intervals",             "intervals": [                 "2022-01-12T10:59:51Z/2024-06-29T10:59:51Z"             ]         },         "queryType": "groupBy",         "granularity": "day"     } }`
+	f, err := Load([]byte(qry))
+
+	assert.Nil(f, "filter should be nil")
+	assert.Nil(err, "error should not be nil")
+}
